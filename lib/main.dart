fc0cc2a1ba3f7ebaa42fa590b547023780fcf1e7
Semyon Polovodov <s4ns4l4ssn4ss@mail.ru>
2025-05-04 11:12:32 +0500

    Merge remote-tracking branch 'origin/main'

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() async {
   try {
     print('Initializing MapKit...');
     // init.initMapkit(apiKey: 'e9260d12-497f-41a4-b878-d38601b3639d');
-    await init.initMapkit(apiKey: 'b9296eef-e8fc-4109-b187-d45172699d10');
+    init.initMapkit(apiKey: 'b9296eef-e8fc-4109-b187-d45172699d10');
     print('MapKit initialized');
   } catch (e) {
     print('MapKit initialization failed: $e');
